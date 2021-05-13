df3d6faae6d99a280f0b34e05adee0392dbfd210
Bermejo3 <alberto.bermejo.corredor@gmail.com>
2021-05-13 11:50:27 +0200

    heroku

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const app = express();
-const path = require('path')
 
-app.use(express.static(path.join(__dirname, 'dist/Portfolio')));
+app.use(express.static(__dirname + '/dist/Portfolio'));
 app.get('/*', function(req, res){
-    res.sendFile("index.html", {root: path.join(__dirname, 'dist/Portfolio')});
+    res.sendFile("index.html", {root: __dirname + '/dist/Portfolio'});
 })
 
 app.listen(process.env.PORT || 8080)
